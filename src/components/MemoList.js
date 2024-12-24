bea80934caf8bb8b469ae2fd63e66b16ff080b2b
kutimiti1234 <tarbit.offensive@gmail.com>
2024-12-24 22:11:18 +0900

    fix: ensure that you can not add a memo when you don't log in

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useLogIn } from '../hooks/useLogIn';
 
 export function MemoList({ memos, onAddMemo, onSelectMemo }) {
   const memoItems = memos.map((memo) => (
@@ -6,10 +7,19 @@ export function MemoList({ memos, onAddMemo, onSelectMemo }) {
       {memo.content.split('\n')[0]}
     </li>
   ));
+
+  const { isLoggedIn } = useLogIn();
   return (
     <>
       <ul>{memoItems}</ul>
-      <p className="add-memo" onClick={onAddMemo}>
+      <p
+        className={isLoggedIn ? 'add-memo' : 'add-memo-disabled'}
+        onClick={() => {
+          if (isLoggedIn === true) {
+            onAddMemo();
+          }
+        }}
+      >
         +
       </p>
     </>
